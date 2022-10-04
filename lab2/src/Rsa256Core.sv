b5943ebbd6106ec823454d0afd7f800eb2896891
wdsjiow <63343649+wdsjiow@users.noreply.github.com>
2022-10-04 16:58:18 +0800

    Add files via upload
    new core

--- lab2/src/Rsa256Core.sv
+++ lab2/src/Rsa256Core.sv
@@ -1,4 +1,3 @@
-
 module Rsa256Core (
     input          i_clk,
     input          i_rst,
@@ -12,286 +11,372 @@ module Rsa256Core (
 
   // operations for RSA256 decryption
   // namely, the Montgomery algorithm
+  parameter S_IDLE = 2'd0;
+  parameter S_PREP = 2'd1;
+  parameter S_MONT = 2'd2;
+  parameter S_CALC = 2'd3;
 
-endmodule
+  logic [1:0] state_r, state_w;
+  logic [255:0] o_m_r, o_m_w;
+  logic [255:0] t_r, t_w;
+  logic [15:0] count_r, count_w;
 
-module Mont(
-    input		   i_clk,
-    input 		   i_rst,
-    input  [255:0] i_a, // cipher text y
-    input  [255:0] i_d, // private key
-    input  [255:0] i_n,
-    output [257:0] o_m
-  );
-  logic [257:0] o_m_r, o_m_w;
-  logic [257:0] t_r, t_w;
-  logic [15:0] count_r, count_w;  //counter from 0 to 255
   // ModuloProduct output wire
-  logic [257:0] initial_t;
-  // Mont_m output wire
-  logic [257:0] o_Mont_m;
-  // Mont_m output wire
-  logic [257:0] o_Mont_t;
+  logic [255:0] initial_t;
+  logic mod_finish;
+  // Mont_m, Mont_t input wire
+  logic start_mont_w, start_mont_r;
+  logic [255:0] Mont_m_a_r, Mont_m_a_w;
+  logic [255:0] Mont_t_a_r, Mont_t_a_w;
+  // Mont_m, Mont_t output wire
+  logic [255:0]   o_Mont_m, o_Mont_t;
+  logic           o_Mont_m_finish, o_Mont_t_finish;
 
-  assign o_m = o_m_r;
+  assign o_finished = (state_r == S_CALC && count_r == 16'd256) ? 1'b1 : 1'b0;
+  assign o_a_pow_d = o_m_r;
 
   ModuloProduct mod0(
                   .i_clk(i_clk),
                   .i_rst(i_rst),
-                  .n(i_n),
-                  .i_a(2**256), //2**256, do we need to specify 256 bits?
-                  .i_b(i_a),
-                  .i_k(256'd256),
-                  .o_m(initial_t)
-                );
-  MontgemoryAlgorithm Mont_m(
-                        .i_clk(i_clk),
-                        .i_rst(i_rst),
-                        .i_N(i_n),
-                        .i_a(o_m_r),
-                        .i_b(t_r),
-                        .o_m(o_Mont_m)
-                      );
-  MontgemoryAlgorithm Mont_t(
-                        .i_clk(i_clk),
-                        .i_rst(i_rst),
-                        .i_N(i_n),
-                        .i_a(t_r),
-                        .i_b(t_r),
-                        .o_m(o_Mont_t)
-                      );
-  // combinational circuits
+                  .i_start(i_start),
+                  .i_y(i_a),
+                  .i_n(i_n),
+                  .mod_output(initial_t),
+                  .o_finished(mod_finish));
+
+  MontgemoryAlgorithm  Mont_m(
+                         .i_clk(i_clk),
+                         .i_start(start_mont_r),
+                         .i_rst(i_rst),
+                         .i_N(i_n),
+                         .i_a(Mont_m_a_w),
+                         .i_b(t_w),
+                         .o_m(o_Mont_m),
+                         .o_finished(o_Mont_m_finish));
+
+  MontgemoryAlgorithm  Mont_t(
+                         .i_clk(i_clk),
+                         .i_start(start_mont_r),
+                         .i_rst(i_rst),
+                         .i_N(i_n),
+                         .i_a(Mont_t_a_w),
+                         .i_b(t_w),
+                         .o_m(o_Mont_t),
+                         .o_finished(o_Mont_t_finish));
+
+  //combinational circuits
   always_comb
   begin
-    if(count_r < 16'd256)
-    begin
-      if (i_d[count_r] == 1'd1)
-      begin // count_r-th bit of d is 1
-        o_m_w = o_Mont_m;
-      end
-      else
+    state_w = state_r;
+    count_w = count_r;
+    start_mont_w = start_mont_r;
+    o_m_w = o_m_r;
+    t_w = t_r;
+    Mont_m_a_w = Mont_m_a_r;
+    Mont_t_a_w = Mont_t_a_r;
+
+    case (state_r)
+      S_IDLE:
       begin
-        o_m_w = o_m_r;
+        if(i_start)
+        begin
+          state_w = S_PREP;
+          count_w = 0 ;
+        end
       end
-      t_w = o_Mont_t;
-      count_w = count_r + 16'd1;
-    end
-    else
-    begin
-      o_m_w = o_m_r;
-      t_w = t_r;
-      count_w = count_r ;
-    end
-  end
-  // sequential circuits
-  always_ff @(posedge i_clk or negedge i_rst)
-  begin
-    // reset
-    if (!i_rst_n)
-    begin
-      t_r <= initial_t; // # of bits are different
-      o_m_r <= 258'd1;
-      count_r <= 16'd0;
-    end
-    else
-    begin
-      t_r <= t_w;
-      o_m_r <= o_m_w;
-      count_r <= count_w;
-    end
-  end
-endmodule
-
-module ExponentiationSquaring(
-    input i_clk,
-    input i_rst,
-    input [255:0] y,
-    input [255:0] d, // private key
-    input [255:0] n,
-    output [257:0] o_m
-  );
-  logic [257:0] o_m_r, o_m_w;
-  logic [257:0] t_r, t_w;
-  logic [15:0] count_r, count_w;  //counter from 0 to 255
-
-  assign o_m = o_m_r;
-
-  // combinational circuits
-  always_comb
-  begin
-    if(count_r < 16'd256)
-    begin
-      if (d[count_r] == 1'd1)
-      begin // count_r-th bit of d is 1
-        o_m_w = o_m_r * t_r % n ;//m = m*t(mod n)
-      end
-      else
+      S_PREP:
       begin
-        o_m_w = o_m_r;
+        if(mod_finish)
+        begin
+          t_w = initial_t ;
+          o_m_w = 1 ;
+          state_w = S_MONT;
+          Mont_m_a_w = t_w;
+          if(i_d[0])
+            Mont_t_a_w = o_m_w;
+          start_mont_w = 1 ;
+        end
+        else
+        begin
+          state_w = state_r;
+        end
       end
-      t_w = (t_r**2) % n;
-      count_w = count_r + 16'd1;
-    end
-    else
-    begin
-      o_m_w = o_m_r;
-      t_w = t_r;
-      count_w = count_r;
-    end
-  end
+      S_MONT:
+      begin
+        //
+        start_mont_w = 0;
+        if(o_Mont_m_finish)
+        begin
+          state_w = S_CALC;
+          count_w = count_r + 1 ;
+          if (i_d[count_r])
+            o_m_w = o_Mont_t ;
+          t_w = o_Mont_m ;
+        end
+        else
+          state_w = state_r ;
 
-  // sequential circuits
-  always_ff @(posedge i_clk or negedge i_rst)
+      end
+      S_CALC:
+      begin
+        if(count_r < 16'd256)
+        begin
+          state_w= S_MONT ;
+          start_mont_w = 1 ;
+          if(i_d[count_r])
+            Mont_t_a_w=o_m_r;
+          Mont_m_a_w=t_r;
+        end
+        else
+        begin
+          state_w = S_IDLE;
+        end
+      end
+
+      default
+      begin
+        state_w = 0;
+        count_w = 0;
+        start_mont_w = 0;
+        o_m_w = 1;
+        t_w = 0;
+        Mont_m_a_w = 1;
+        Mont_t_a_w = 0;
+      end
+    endcase
+  end
+  //sequential circuits
+  always_ff @( posedge i_clk or posedge i_rst )
   begin
-    // reset
-    if (!i_rst)
+    if (i_rst)
     begin
-      t_r <= {2'b0, y}; // # of bits are different
-      o_m_r <= 258'd1;
-      count_r <= 16'd0;
+      state_r <=0;
+      start_mont_r<=0;
+      t_r<=0;
+      o_m_r<=0;
+      count_r<=0;
+      Mont_m_a_r<=0;
+      Mont_t_a_r<=0;
     end
     else
     begin
-      t_r <= t_w;
-      o_m_r <= o_m_w;
-      count_r <= count_w;
+      state_r <=state_w;
+      start_mont_r<=start_mont_w;
+      t_r<=t_w;
+      o_m_r<=o_m_w;
+      count_r<=count_w;
+      Mont_m_a_r<=Mont_m_a_w;
+      Mont_t_a_r<=Mont_t_a_w;
     end
   end
 
 endmodule
 
 module ModuloProduct (
-    input i_clk,
-    input i_rst,
-    input [255:0] n,
-    input [255:0] i_a,
-    input [255:0] i_b,
-    input [255:0] i_k, // k is # of bits of a
-    output [257:0] o_m
+    input   i_clk,
+    input   i_rst,
+    input   i_start,
+    input   [255:0] i_y,
+    input   [255:0] i_n,  // length of bits is 255 = 2^8 -> need a 8-bit count
+    output  [255:0] mod_output,
+    output  o_finished
   );
-  logic [257:0] o_m_r, o_m_w;
-  logic [257:0] t_r, t_w;
-  logic [255:0] count_r, count_w;  //counter from 0 to i_k
 
-  assign o_m = o_m_r;
+  logic state_r, state_w;
+  logic finish_w, finish_r;
+  logic [257:0] output_w, output_r;
+  logic [257:0] t_w, t_r;
+  logic [9:0] count_w, count_r;
 
-  // combinational circuits
+  assign o_finished = finish_r ;
+  assign mod_output = output_r[255:0];
+
   always_comb
   begin
-    if(count_r <= i_k)
-    begin
-      if (i_a[count_r] == 1'd1)
-      begin // count_r-th bit of d is 1
-        o_m_w = ((o_m_r + t_r)>=n) ? (o_m_r + t_r - n) : (o_m_r + t_r);
-      end
-      else
+    state_w = state_r ;
+    output_w = output_r ;
+    t_w = t_r ;
+    count_w = count_r ;
+    finish_w = finish_r ;
+
+    case(state_r)
+      0 :
       begin
-        o_m_w = o_m_r;
+        if(i_start)
+        begin
+          state_w = 1 ;
+          output_w = 0 ;
+          t_w = {2'b0, i_y} ;
+          count_w = 0 ;
+          finish_w = 0 ;
+        end
       end
-      t_w = (t_r*2 > n) ? (2*t_r - n) : (2*t_r);
-      count_w = count_r + 256'd1;
-    end
-    else
-    begin
-      o_m_w = o_m_r;
-      t_w = t_r;
-      count_w = count_r ;
-    end
+
+      1:
+      begin
+        if(t_r+t_r > i_n)
+          t_w = t_r + t_r - i_n ;
+        else
+          t_w = t_r + t_r ;
+        // count == 256 final round is done
+        if(count_r == 10'd256)
+        begin
+          if(output_r+t_r >= i_n)
+          begin
+            output_w = output_r + t_r - i_n ;
+          end
+          else
+          begin
+            output_w = output_r + t_r ;
+          end
+          state_w  = 0 ;
+          finish_w = 1 ;
+        end
+        count_w = count_r + 1 ;
+
+      end
+
+      default
+      begin
+        state_w = 0 ;
+        output_w = 0 ;
+        t_w  = 0 ;
+        count_w = 0 ;
+        finish_w = 0 ;
+      end
+    endcase
   end
 
   // sequential circuits
   always_ff @(posedge i_clk or negedge i_rst)
   begin
-    // reset
-    if (!i_rst)
+    if(i_rst)
     begin
-      t_r <= {2'd0,i_b}; // # of bits are different
-      o_m_r <= 258'd0;
-      count_r <= 256'd0;
+      state_r <= 0;
+      output_r <= 0;
+      t_r <= 0;
+      count_r <= 0;
+      finish_r <= 0;
     end
     else
     begin
+      state_r <= state_w;
+      output_r <= output_w;
       t_r <= t_w;
-      o_m_r <= o_m_w;
       count_r <= count_w;
+      finish_r <= finish_w;
     end
   end
 endmodule
 
-module MontgemoryAlgorithm(
-    input		   i_clk,
-    input 		   i_rst,
-    input  [255:0] i_N,
-    input  [255:0] i_a,
-    input  [255:0] i_b,
-    output [257:0] o_m
+
+module MontgemoryAlgorithm (
+    input   i_clk,
+    input   i_start,
+    input   i_rst,
+    input   [255:0] i_N,  // length of bits is 256 = 2^8 -> need a 8-bit count
+    input   [255:0] i_a,
+    input   [255:0] i_b,
+    output  [255:0] o_m,
+    output  o_finished
   );
 
-  logic  [257:0] o_m_r, o_m_w;
-  logic  [15:0]  count_r, count_w;
+  logic state_w, state_r;
+  logic finish_w, finish_r;
+  logic [257:0] o_m_w, o_m_r;
+  logic [15:0] count_r, count_w;
+  logic [257:0] tmp1;
+  logic [256:0] tmp2;
 
-  assign o_m = o_m_r;
+  assign o_finished = finish_r;
+  assign o_m = o_m_r[255:0] ;
 
   always_comb
   begin
     // Default Value //
-    o_m_w = o_m_r;
-    count_w = count_r;
+    state_w = state_r ;
+    o_m_w = o_m_r ;
+    count_w = count_r ;
+    finish_w = 0 ;
+    tmp1 = o_m_r ;
+    tmp2 = o_m_r ;
 
-    if(count_r < 16'd256)
-    begin
-      //count_w = count_r + 1;
-      if(i_a[count_r] == 1'b1)
+    case(state_r)
+      0 :
       begin
-        if(o_m_r[0] ^ i_b[0] == 1'b1) // m is odd
+        o_m_w = 0 ;
+        count_w = 0 ;
+        finish_w = 0 ;
+        tmp1 = 0 ;
+        tmp2 = 0 ;
+        if(i_start)
         begin
-          o_m_w = (o_m_r + i_b + i_N) >> 1;
+          state_w = 1 ;
+        end
+      end
+
+      1:
+      begin
+        if(i_a[count_r] == 1'b1)
+        begin
+          tmp1 = o_m_r + i_b + ((o_m_r[0]^i_b[0])? i_N:0);
+          o_m_w = (o_m_r + i_b + ((o_m_r[0]^i_b[0])? i_N:0)) >> 1 ;
         end
         else
-          o_m_w = (o_m_r + i_b) >> 1;
-      end
-
-      else
-      begin
-        if(o_m_r[0] == 1'b1)
         begin
-          o_m_w = (o_m_r + i_N) >> 1;
+          tmp1 = o_m_r + ((o_m_r[0])? i_N:0);
+          o_m_w =(o_m_r + ((o_m_r[0])? i_N:0))>>1 ;
+        end
+        tmp2 = tmp1 >> 1 ;
+        //o_m_w = tmp2 ;
+        // count == 255 final round is done
+        // if(&count_r)
+        if(count_r == 16'd255)
+        begin
+          o_m_w = tmp2 - (tmp2>=i_N? i_N : 0) ;
+          /*
+          if(o_m_r >= i_N)
+          begin
+            o_m_w = o_m_r - i_N;
+          end
+          else
+            o_m_w = o_m_r;
+          */
+          state_w  = 0 ;
+          finish_w = 1 ;
         end
         else
-          o_m_w = o_m_r >> 1;
+          count_w = count_r + 1 ;
       end
-      count_w = count_r + 1;
-    end
 
-    else
-    begin
-      count_w = count_r;
-      if(o_m_r >= i_N)
+      default
       begin
-        o_m_w = o_m_r - i_N;
+        state_w = 0 ;
+        o_m_w = 0 ;
+        count_w = 0 ;
+        finish_w = 0 ;
+        tmp1 = 0 ;
+        tmp2 = 0 ;
       end
-      else
-        o_m_w = o_m_r;
-    end
-
+    endcase
   end
 
-
+  // sequential circuits
   always_ff @(posedge i_clk or negedge i_rst)
   begin
-    if(!i_rst)
+    if(i_rst)
     begin
-      count_r <= 16'd0;
-      o_m_r <= 257'd0;
+      state_r <= 0;
+      o_m_r <= 0;
+      count_r <= 0;
+      finish_r <= 0;
     end
-
     else
     begin
-      count_r <= count_w;
+      state_r <= state_w;
       o_m_r <= o_m_w;
+      count_r <= count_w;
+      finish_r <= finish_w;
     end
   end
-
-
 endmodule
-
