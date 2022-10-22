8d1e18cc500d069377c0ba1f71503e24ae7a5a43
melody <b08504045@ntu.edu.tw>
2022-10-22 18:44:25 +0800

    add_tb_init

---
+++ lab3/tb/tb_dsp.sv
@@ -0,0 +1,40 @@
+`timescale 1ns/100ps
+
+module tb;
+	localparam CLK = 10;
+	localparam HCLK = CLK/2;
+
+	logic clk, start, pause, stop, fast, slow, rst_n, daclrck;
+    logic [15:0] data_play;
+    logic [15:0] dac_data;
+    logic [19:0] addr_play;
+
+	initial clk = 0;
+	always #HCLK clk = ~clk;
+
+	AudDSP test(
+	.i_rst_n(rst_n),
+	.i_clk(clk),
+	.i_start(start),
+	.i_pause(pause),
+	.i_stop(stop),
+	.i_fast(fast),
+	.i_slow_0(slow), // constant interpolation
+	.i_slow_1(0), // linear interpolation
+	.i_daclrck(daclrck),
+	.i_sram_data(data_play),
+	.o_dac_data(dac_data),
+	.o_sram_addr(addr_play)
+    );
+
+
+
+initial begin
+	$fsdbDumpfile("dsp_test.fsdb");
+	$fsdbDumpvars;	
+	
+end
+
+initial #(CLK*500000) $finish;
+
+endmodule
---
+++ lab3/tb/tb_ic2.sv
@@ -0,0 +1,37 @@
+`timescale 1ns/100ps
+
+module tb;
+	localparam CLK = 10;
+	localparam HCLK = CLK/2;
+
+	logic bclk, rst_n, start, pause, stop, adclrck;
+    logic [15:0] adc_data;
+    logic [15:0] data_record;
+    logic [19:0] addr_record;
+    
+	initial clk = 0;
+	always #HCLK clk = ~clk;
+
+	AudRecorder recorder0(
+	.i_rst_n(rst_n), 
+	.i_clk(bclk),
+	.i_lrc(adclrck),
+	.i_start(start),
+	.i_pause(pause),
+	.i_stop(stop),
+	.i_data(adc_data),
+	.o_address(addr_record),
+	.o_data(data_record),
+);
+
+
+
+initial begin
+	$fsdbDumpfile("ic2_test.fsdb");
+	$fsdbDumpvars;	
+	
+end
+
+initial #(CLK*500000) $finish;
+
+endmodule
