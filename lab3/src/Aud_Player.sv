38b47260c91b85d8c4fa4fd9c922d3a63171a9cc
wdsjiow <63343649+wdsjiow@users.noreply.github.com>
2022-10-31 18:03:38 +0800

    Update Aud_Player.sv

--- lab3/src/Aud_Player.sv
+++ lab3/src/Aud_Player.sv
@@ -7,58 +7,63 @@ module AudPlayer (
     output o_aud_dacdat
   );
 
-  logic data_w, data_r;
+  logic dac_data_w, dac_data_r;
+  logic [4:0] count_w, count_r;
+
   logic [1:0] state_w, state_r;
-  logic [4:0] counter_w, counter_r;
-  logic [15:0] in_data_w, in_data_r;
+
+
+  logic [15:0] in_dac_data_w, in_dac_data_r;
   logic lrc_negedge_w, lrc_negedge_r;
-  //logic input_taken;
 
-  parameter S_IDLE = 2'b00;
-  parameter S_LRC1 = 2'b01;
-  parameter S_PLAY = 2'b10;
 
-  assign o_aud_dacdat = data_r;
+  parameter S_IDLE = 2'd0;
+  parameter S_WAIT = 2'd1;
+  parameter S_PLAY = 2'd2;
+
+  assign o_aud_dacdat = dac_data_r;
 
 
   always_comb
   begin
-    data_w = data_r;
+    dac_data_w = dac_data_r;
     state_w = state_r;
-    counter_w = counter_r;
+    count_w = count_r;
     lrc_negedge_w = i_daclrck;
-    in_data_w = in_data_r;
+    in_dac_data_w = in_dac_data_r;
 
     case(state_r)
       S_IDLE:
       begin
         if (i_en)
         begin
-          state_w = S_LRC1;
+          state_w = S_WAIT;
         end
       end
 
-      S_LRC1:
+      S_WAIT:
       begin
-        in_data_w = i_dac_data;
+        in_dac_data_w = i_dac_data;
         if(!i_daclrck && lrc_negedge_r)
         begin
           state_w = S_PLAY;
-          counter_w = 5'b0;
-          data_w = in_data_r[15];
-          in_data_w = in_data_r << 1;
+          count_w = 5'b0;
+          dac_data_w = i_dac_data_r[4'd15-count_r];
+          //dac_data_w = in_dac_data_r[15];
+          //in_dac_data_w = in_dac_data_r << 1;
         end
       end
 
       S_PLAY:
       begin
-        counter_w = counter_r + 1'b1;
-        data_w = in_data_r[15];
-        in_data_w = in_data_r << 1;
-        if (counter_r >= 5'd15)
+        count_w = count_r + 1'b1;
+        dac_data_w = i_dac_data_r[4'd15-count_r];
+        //dac_data_w = in_dac_data_r[15];
+        //in_dac_data_w = in_dac_data_r << 1;
+        if (count_r >= 5'd15)
         begin
-          state_w = S_LRC1;
-          in_data_w = 16'b0;
+          state_w = S_WAIT;
+          in_dac_data_w = 16'b0;
         end
       end
       default:
@@ -72,17 +77,17 @@ module AudPlayer (
     if (!i_rst_n)
     begin
       state_r <= S_IDLE;
-      data_r <= 0;
-      counter_r <= 5'd15;
-      in_data_r <= 0;
+      dac_data_r <= 0;
+      count_r <= 5'd15;
+      in_dac_data_r <= 0;
       lrc_negedge_r <= 0;
     end
     else
     begin
       state_r <= state_w;
-      data_r <= data_w;
-      counter_r <= counter_w;
-      in_data_r <= in_data_w;
+      dac_data_r <= dac_data_w;
+      count_r <= count_w;
+      in_dac_data_r <= in_dac_data_w;
       lrc_negedge_r <= lrc_negedge_w;
     end
 
